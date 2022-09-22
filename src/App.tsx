f43be9c4f5f8af0a6b24b2979aa77f7432c8f74b
athosmpugliese <athospu@gmail.com>
2022-09-22 12:28:40 -0300

    finish

---
+++ src/App.tsx
@@ -0,0 +1,86 @@
+import { useState } from 'react'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+import TaskForm from './Components/TaskForm'
+import TaskList from './Components/TaskList'
+import Modal from './Components/Modal'
+import { ITask } from './Interfaces/Task'
+import styles from './App.module.css'
+
+
+function App() {
+  const [taskList, setTaskList] = useState<ITask[]>([]);
+  const [taskToUpdate, setTaskToUpdate] = useState<ITask | null>(null);
+
+  const deleteTask = (title: string): void => {
+    setTaskList(
+      taskList.filter((task) => {
+        return task.title !== title;
+      })
+    );
+  };
+
+  const hideOrShowModal = (display: boolean) => {
+    const modal = document.getElementById("modal");
+    if (display) {
+      modal!.classList.remove("hide");
+    } else {
+      modal!.classList.add("hide");
+    }
+  };
+
+  const editTask = (task: ITask): void => {
+    hideOrShowModal(true);
+    setTaskToUpdate(task);
+  };
+
+  const updateTask = (id: number, title: string, difficulty: number) => {
+    const updatedTask: ITask = { id, title, difficulty };
+
+    const updatedItems = taskList.map((task) => {
+      return task.id === updatedTask.id ? updatedTask : task;
+    });
+
+    setTaskList(updatedItems);
+
+    hideOrShowModal(false);
+  };
+
+  return (
+    <div>
+      <Modal
+        title="Editar tarefa"
+        children={
+          <TaskForm
+            btnText="Editar"
+            taskList={taskList}
+            task={taskToUpdate}
+            handleUpdate={updateTask}
+          />
+        }
+      />
+      <Header />
+      <main className={styles.main}>
+        <div className={styles.todo_form}>
+          <h2>O que você vai fazer?</h2>
+          <TaskForm
+            taskList={taskList}
+            setTaskList={setTaskList}
+            btnText="Cadastrar"
+          />
+        </div>
+        <div className="todo-container">
+          <h2>Suas tarefas:</h2>
+          <TaskList
+            taskList={taskList}
+            handleDelete={deleteTask}
+            handleEdit={editTask}
+          />
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
